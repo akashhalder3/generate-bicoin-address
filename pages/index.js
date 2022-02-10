63bf0e9d9a11b1490c24df9f99f90b339b7a79ef
akashhalder3 <mailmeakashhalder@gmail.com>
2022-02-10 20:38:49 +0530

    fixed async issue

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import * as Bip39 from 'bip39';
-import * as Hdkey from 'hdkey';
+import * as bip39 from 'bip39';
+import * as hdkey from 'hdkey';
 import * as createHash from 'create-hash';
 import * as bs58check from 'bs58check';
 import React, { useState } from 'react';
@@ -14,30 +14,30 @@ export default function Home() {
 
   const bitcoinWallet = async () => 
   { 
-    // wrap logic into a async function
-    //1. Master Private Key Generation
-    const mnemonic = Bip39.generateMnemonic(); // generates a 12 word mnemonic
-    const seed = Bip39.mnemonicToSeed(mnemonic) //create seed buffer  
-    console.log(mnemonic);
-    setMnemonic(mnemonic)
-    //2. Address generation
-    const root = Hdkey.fromMasterSeed(seed);  // should not be passing a promise into here
+    const mnemonic = bip39.generateMnemonic()
+    //const mnemonic = "gentle mutual speak consider mandate kingdom cash explain soul exile cabin squeeze";
+    setMnemonic(mnemonic);
+    const seed = await bip39.mnemonicToSeed(mnemonic); //creates seed buffer
+    console.log('Seed: ' + seed);
+    console.log('mnemonic: ' + mnemonic);
+
+    const root = hdkey.fromMasterSeed(seed);
     const masterPrivateKey = root.privateKey.toString('hex');
-    //3. add path
-    const addrnode = root.derive("m/44'/60'/0'/0/0");
-    //4. get public key
+    console.log('masterPrivateKey: ' + masterPrivateKey);
+
+    const addrnode = root.derive("m/44'/0'/0'/0/0");
+    console.log('addrnodePublicKey: '+ addrnode._publicKey)
+
     const step1 = addrnode._publicKey;
-    //5.perform sha-256 hashing on public key
     const step2 = createHash('sha256').update(step1).digest();
-    //6. perform RIPEMD hashing on sha-256
     const step3 = createHash('rmd160').update(step2).digest();
-    //7. Add version byte in front of RIPEMD-160 hash (0x00 for mainnet, 0x6f for testnet)
+
     var step4 = Buffer.allocUnsafe(21);
-    step4.writeUInt8(0x6f, 0);
-    step3.copy(step4, 1) //step3 now holds the extended RIPEMD160 result
-    var step9 = bs58check.encode(step4); //Steps 5-9
-    console.log(step9)
-    setWallet(step9)
+    step4.writeUInt8(0x00, 0);
+    step3.copy(step4, 1); //step4 now holds the extended RIPMD-160 result
+    const step9 = bs58check.encode(step4);
+    setWallet(step9);
+    console.log('Base58Check: ' + step9);
   };
 
   return (
@@ -72,7 +72,7 @@ export default function Home() {
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <p className="mt-3 font-extrabold text-dark-600 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Wallet: 0x6f{wallet}
+                  Wallet: {wallet}
                 </p>
                 <p className="mt-3 font-extrabold text-indigo-600 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Mnemonic: {mnemonic}
